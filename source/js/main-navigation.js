83682acde676740e0dad88272d987930e5994f3b
hawkf <fedor.gorokhov@gmail.com>
2021-01-12 17:35:22 +0200

    Сделал доработки

--- source/js/main-navigation.js
+++ source/js/main-navigation.js
@@ -1,12 +1,13 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 var modal = document.querySelector('.modal');
-var modal_overlay = document.querySelector('.modal-overlay');
-var product_week = document.querySelector('.product-week__button');
-var modal__button = document.querySelector('.modal__button');
-var production_process__button = document.querySelector('.production-process__button');
+var modalOverlay = document.querySelector('.modal-overlay');
+var productWeek = document.querySelector('.product-week__button');
+var modalButton = document.querySelector('.modal__button');
+var productionProcessButton = document.querySelector('.production-process__button');
 
 navMain.classList.remove('main-nav--nojs');
+navMain.classList.add('main-nav--closed');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
@@ -18,17 +19,23 @@ navToggle.addEventListener('click', function () {
   }
 });
 
-product_week.addEventListener('click', function () {
-  modal.classList.remove("visually-hidden");
-  modal_overlay.classList.remove("visually-hidden");
-});
+if(productWeek != null) {
+  productWeek.addEventListener('click', function () {
+    modal.classList.remove("visually-hidden");
+    modalOverlay.classList.remove("visually-hidden");
+  });
+}
 
-production_process__button.addEventListener('click', function () {
-  modal.classList.remove("visually-hidden");
-  modal_overlay.classList.remove("visually-hidden");
-});
+if(productionProcessButton != null) {
+  productionProcessButton.addEventListener('click', function () {
+    modal.classList.remove("visually-hidden");
+    modalOverlay.classList.remove("visually-hidden");
+  });
+}
 
-modal__button.addEventListener('click', function () {
-  modal.classList.add("visually-hidden");
-  modal_overlay.classList.add("visually-hidden");
-});
+if(modalButton != null) {
+  modalButton.addEventListener('click', function () {
+    modal.classList.add("visually-hidden");
+    modalOverlay.classList.add("visually-hidden");
+  });
+}
